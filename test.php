7057b60655317074ede67eb131828b9f52ae4d87
aliriyazr1 <aliriyazr1@gmail.com>
2022-11-20 18:12:52 +0530

    initialized git repository

---
+++ test.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head></head>
+    <body><?php $var=1;
+        if ($var>2){echo "<p>"."HI"."</p>"}
+        else {echo "<p>"."BYEE"."</p>"} ?></body>
+</html>
